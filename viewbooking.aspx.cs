be89c416b50a26eacf048aad71385a5b09b34d38
bipinprajapati0712 <36466751+bipinprajapati0712@users.noreply.github.com>
2018-02-26 17:55:29 +0530

    Create viewbooking.aspx.cs

---
+++ viewbooking.aspx.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Bookingoperation;
+
+namespace hotelmanagement
+{
+    public partial class ViewBookings : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string roomcat = DropDownList1.SelectedItem.Text;
+            Booking obj = new Booking();
+            GridView1.DataSource = obj.view(roomcat);
+            GridView1.DataBind();
+        }
+
+    }
+}
